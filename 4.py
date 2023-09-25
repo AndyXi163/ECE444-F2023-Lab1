e3c966c3323c8ad155554cf76148d204424d4278
An <andyxi163@gmail.com>
2023-09-24 23:53:33 -0400

    Ran exercise 4 successfully.

--- 4.py
+++ 4.py
@@ -6,29 +6,32 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'ThisNThatECE444'
+
 moment = Moment(app)
+
 class NameForm(FlaskForm):
- name = StringField('What is your name?', validators=[DataRequired()])
- submit = SubmitField('Submit')
+    name = StringField('What is your name?', validators=[DataRequired()])
+    email = StringField('What is your UofT email address?', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     form = NameForm()
     if form.validate_on_submit():
         old_name = session.get('name')
+        old_email = session.get('email')
         if old_name is not None and old_name != form.name.data:
             flash('Looks like you have changed your name!')
+        if old_email is not None and old_email !=form.email.data:
+            flash('Looks like you have changed your email!')
         session['name'] = form.name.data
+        session['email'] = form.email.data
         return redirect(url_for('index'))
-    return render_template('index.html', form = form, name = session.get('name'), current_time=datetime.utcnow())
-@app.route('/user/<name>')
-def user(name):
-    name = None
-    form = NameForm()
-    if form.validate_on_submit():
-        name = form.name.data
-        form.name.data = ''
-    return render_template('user.html', form=form, name=name, current_time=datetime.utcnow())
+    return render_template('index_4.html', form = form, name = session.get('name'), email = session.get('email'), 
+        current_time=datetime.utcnow())
+
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
